0a451ea0219177caf644211b7ce94d24a8b8a6ab
xunhanliu <1638081534@qq.com>
2018-12-08 12:43:22 +0800

    12-8

--- js/layoutCtl.js
+++
@@ -1,9 +0,0 @@
-//调整层级
-$(".bottom-gallery").click(function(){
-    $(".bottom-gallery").css("z-index",1001);
-});
-
-$(".middle").click(function(){
-    $(".bottom-gallery").css("z-index",0);
-});
-
